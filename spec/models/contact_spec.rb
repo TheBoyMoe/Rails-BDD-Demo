9ed9525514cbdfc885761ee38b084e79abef0b72
theboymo <billfero@gmail.com>
2018-02-07 18:49:38 +0000

    refactor model spec, move factory bot example into a separate group

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-	before {@contact = build(:contact)}
 
 	context 'validate model attributes' do
-    it {is_expected.to validate_presence_of(:name)}
-    it {is_expected.to validate_presence_of(:address)}
-    it {is_expected.to validate_presence_of(:phone_number)}
-    it {is_expected.to validate_length_of(:phone_number).is_at_least(10)}
+		it {is_expected.to validate_presence_of(:name)}
+		it {is_expected.to validate_presence_of(:address)}
+		it {is_expected.to validate_presence_of(:phone_number)}
+		it {is_expected.to validate_length_of(:phone_number).is_at_least(10)}
+	end
+
+	context 'validate factory bot implementation' do
+		before {@contact = create(:contact)}
 
 		it "is a valid user" do
 			expect(@contact).to be_valid
