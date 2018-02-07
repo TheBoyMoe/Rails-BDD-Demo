e4b4a2dfbca8f500071c4f68a61bb5568e49f0cb
theboymo <billfero@gmail.com>
2018-02-07 15:21:32 +0000

    enable interactive debugging using the byebug gem and prepare the database

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'shoulda/matchers'
 require 'support/factory_bot'
+require 'byebug'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -37,7 +38,27 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+	config.use_transactional_fixtures = false
+
+	config.before(:suite) do
+		DatabaseCleaner.clean_with(:truncation)
+	end
+
+	config.before(:each) do
+		DatabaseCleaner.strategy = :transaction
+	end
+
+	config.before(:each, :js => true) do
+		DatabaseCleaner.strategy = :truncation
+	end
+
+	config.before(:each) do
+		DatabaseCleaner.start
+	end
+
+	config.after(:each) do
+		DatabaseCleaner.clean
+	end
 
 
   config.infer_spec_type_from_file_location!
