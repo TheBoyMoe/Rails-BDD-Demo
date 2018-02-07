cb42e55f2f32bcf4bc1de40128dfa4343b5bf55f
theboymo <billfero@gmail.com>
2018-02-07 20:19:58 +0000

    impl contact page steps

--- features/step_definitions/contact_page_steps.rb
+++ features/step_definitions/contact_page_steps.rb
@@ -1,12 +1,13 @@
 Given(/^there is a contact with name "([^"]*)" the address "([^"]*)" and phone number "([^"]*)"$/) do |name, address, phone_number|
-	pending # Write code here that turns the phrase above into concrete actions
+	@contact = FactoryBot.create(:contact, name: name, address: address, phone_number: phone_number)
 end
 
 When(/^I am on the "([^"]*)" page of "([^"]*)"$/) do |page, name|
-	pending # Write code here that turns the phrase above into concrete actions
+	@contact = Contact.find_by_name(name)
+	visit "/contacts/#{@contact.id}"
 end
 
-Then(/^I should see "([^"]*)"$/) do |attribute|
-	pending # Write code here that turns the phrase above into concrete actions
+Then(/^I should see "([^"]*)"$/) do |string|
+	expect(page).to have_text(string)
 end
 
