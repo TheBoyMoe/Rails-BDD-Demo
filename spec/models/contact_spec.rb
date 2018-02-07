25ee84dd00eaedc21c2725cc364e5941280c5378
theboymo <billfero@gmail.com>
2018-02-07 18:06:50 +0000

    test factory bot and rspec implemntation

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  it {is_expected.to validate_presence_of(:name)}
-  it {is_expected.to validate_presence_of(:address)}
-  it {is_expected.to validate_presence_of(:phone)}
-  it {is_expected.to validate_length_of(:phone).is_at_least(10)}
+	before {@contact = build(:contact)}
+
+	context 'validate model attributes' do
+    it {is_expected.to validate_presence_of(:name)}
+    it {is_expected.to validate_presence_of(:address)}
+    it {is_expected.to validate_presence_of(:phone)}
+    it {is_expected.to validate_length_of(:phone).is_at_least(10)}
+
+		it "is a valid user" do
+			expect(@contact).to be_valid
+		end
+	end
+
 end
